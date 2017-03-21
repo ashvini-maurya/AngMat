0db47fabb3c3de9f53dc111e86a918fdc6c25ff2
Ashvini Kumar <ashvinikumar45@gmail.com>
2017-03-21 14:03:50 +0530

    Routing working

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './app.routes';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    RoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes, RouterModule } from '@angular/router';
+ import {NgModule} from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+	{path:'', redirectTo:'/login', pathMatch:'full'},
+	{path: 'login', component:LoginComponent},
+	{path: 'home', component:HomeComponent},
+	
+];
+
+// export class Routing {
+// 	RouterModule.forRoot(routes); 
+// } 
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+
+export class RoutingModule{}
